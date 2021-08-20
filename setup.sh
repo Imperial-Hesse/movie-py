9a96601e039ca5a5e606284f49a31aa62d783174
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-08-20 14:49:07 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+echo '[*] Prepare for install dependancies '
+sleep 1 
+echo 3....
+sleep 1 
+echo 2....
+sleep 1
+echo 1....
+
+sudo apt-get install update 
+sudo apt-get dist-upgrade
+pip install colorama 
+pip install imdb
+pip install pip install IMDbPY
+pip install git+https://github.com/alberanid/imdbpy
+pip install imdbpy
+
+
