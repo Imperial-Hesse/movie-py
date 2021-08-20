aeece43ff20001429f402881290977fbf7ca1b69
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-08-20 17:57:53 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,11 +5,15 @@ echo 'prapre for install dependcencies'
 
 cls
 echo 1...
-timeout 1 
+timeout /t 1 /nobreak > NUL
 echo 2...
-timeout 2 
+timeout /t 1 /nobreak > NUL
 echo 3...
-timout 1 
+timeout /t 1 /nobreak > NUL
 
 pip install colorama 
+pip install imdb
+pip install pip install IMDbPY
+pip install git+https://github.com/alberanid/imdbpy
+pip install imdbpy 
 
