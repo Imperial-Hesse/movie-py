7083d01507277e5e9ad49250919499f8a9e74c61
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-08-20 14:49:23 -0400

    Update movie.py

--- movie.py
+++ movie.py
@@ -1,20 +1,37 @@
 import os 
-import time
+import time as t 
 import imdb
 import colorama 
+import datetime as datetime
+from datetime import datetime
+from imdb import IMDb
 from colorama import Fore 
 from colorama import init
 
-init()
+def cs(X):
+	t.sleep(X)
+	os.system(' clear ')
 
+cs(2)
+
+init()
 
 ia = imdb.IMDb()
 
 
+def banner():
+	print(Fore.BLUE+"")
+	os.system('figlet cin-dump ')
+	print(Fore.RED+"            V1.0 ")
+	print(Fore.RED+" Dumpster diving for movies :D")
+	
+
 search = ia.get_top250_movies()
 
+banner()
 for i in range(250):
-    time.sleep(0.1)
-    print(Fore.RED+".............")
-    print(Fore.BLUE+"")
-    print(search[i])
+	                #[DATA] ==> | The Lord of the Rings: The Fellowship of the Ring
+	print(Fore.RED+"--------------------------------------------------------------|")
+	t.sleep(0.3)
+	print(Fore.BLUE+'[DATA]  ==>  | ' + str(datetime.now()))
+	print(Fore.YELLOW+'[MOVIE] ==> |', search[i],)
